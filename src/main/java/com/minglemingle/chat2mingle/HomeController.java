45d84c2f99c5c1c3f55b8b14adc7e4312cee571c
Daniel Choi <schoiak@connect.ust.hk>
2023-05-22 14:58:36 +0900

    initial commit

---
+++ src/main/java/com/minglemingle/chat2mingle/HomeController.java
@@ -0,0 +1,38 @@
+package com.minglemingle.chat2mingle;
+
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) throws UnsupportedEncodingException {
+        Logger log = LogManager.getLogger("case3");
+        log.debug("debug message 출력");
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+        String formattedDate = dateFormat.format(date);
+
+        model.addAttribute("serverTime", formattedDate );
+
+        return "home";
+    }
+
+}
