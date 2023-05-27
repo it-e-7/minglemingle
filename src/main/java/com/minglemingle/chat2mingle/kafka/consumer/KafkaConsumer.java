6c20ee51d5e4b8ffc51b896e164e3471eb28617a
Daniel Choi <schoiak@connect.ust.hk>
2023-05-27 16:12:32 +0900

    Refactor: MessageDTO 파싱 코드 분리
    MessageDTO 파싱코드를 MessageService로 이동

--- src/main/java/com/minglemingle/chat2mingle/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/minglemingle/chat2mingle/kafka/consumer/KafkaConsumer.java
@@ -1,11 +1,7 @@
 package com.minglemingle.chat2mingle.kafka.consumer;
 
-import com.minglemingle.chat2mingle.message.service.MessageParser;
 import com.minglemingle.chat2mingle.message.service.MessageService;
-import com.minglemingle.chat2mingle.message.vo.MessageDTO;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +10,24 @@ class KafkaConsumer {
 
     private final MessageService messageService;
 
-    private final MessageParser messageParser;
 
-    public KafkaConsumer(MessageService messageService, MessageParser messageParser) {
+    public KafkaConsumer(MessageService messageService) {
         this.messageService = messageService;
-        this.messageParser = messageParser;
     }
 
 //    public void parseAndSendMessage
 
-    @KafkaListener(topics = "my-topic")
-    public void listenWithHeaders(
-            @Payload String message,
-            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-        MessageDTO messageDto = messageParser.toDto(message);
-
-        messageService.broadcast(messageDto);
+    @KafkaListener(topics = "channel1")
+    public void listenerChannel1(@Payload String message) {
+        messageService.broadcast(message);
+    }
+    @KafkaListener(topics = "channel2")
+    public void listenerChannel2(@Payload String message) {
+        messageService.broadcast(message);
+    }
+    @KafkaListener(topics = "channel3")
+    public void listenerChannel3(@Payload String message) {
+        messageService.broadcast(message);
     }
 
 
