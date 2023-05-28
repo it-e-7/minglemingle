a8c18c4efc1f5a228016b2bb72c99ae747015fb9
Daniel Choi <schoiak@connect.ust.hk>
2023-05-28 14:51:46 +0900

    Feat: 웹소켓 채널 추가/삭제 기능 구현
    웹소켓 채널 추가/삭제 기능 구현

--- src/main/java/com/minglemingle/chat2mingle/websocket/handler/WebSocketHandler.java
+++ src/main/java/com/minglemingle/chat2mingle/websocket/handler/WebSocketHandler.java
@@ -1,6 +1,5 @@
 package com.minglemingle.chat2mingle.websocket.handler;
 
-import com.minglemingle.chat2mingle.kafka.KafkaConst;
 import com.minglemingle.chat2mingle.kafka.producer.KafkaProducer;
 import com.minglemingle.chat2mingle.websocket.service.WebSocketSessionManager;
 import lombok.NonNull;
@@ -9,6 +8,8 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
@@ -26,16 +27,36 @@ public class WebSocketHandler extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(@NonNull WebSocketSession session,
                                   @NonNull TextMessage message) throws Exception {
-        kafkaProducer.sendMessage(KafkaConst.GROUP_ID, message);
+        kafkaProducer.sendMessage(message);
     }
     @Override
     public void afterConnectionEstablished(@NonNull WebSocketSession session) throws Exception {
-        webSocketSessionManager.add(2, session);
+        if (session.getUri() == null) {
+            // do something to delete session
+            return;
+        }
+        UriComponents uriComponents = UriComponentsBuilder.fromUri(session.getUri()).build();
+
+        // Retrieve the query string parameter value
+        String channelFromQuery = uriComponents.getQueryParams().getFirst("channel");
+        if (channelFromQuery == null) {
+            // do something to delete session
+            // invalid Access
+            return;
+        }
+        Integer channel = Integer.valueOf(channelFromQuery);
+
+        session.getAttributes().put("channel", channel);
+        webSocketSessionManager.add(channel, session);
 
     }
     @Override
     public void afterConnectionClosed(@NonNull WebSocketSession session,
                                       @NonNull CloseStatus status) throws Exception {
-        webSocketSessionManager.remove(2, session);
+        Integer channel = (Integer) session.getAttributes().get("channel");
+        if (channel == null) {
+            return;
+        }
+        webSocketSessionManager.remove(channel, session);
     }
 }
