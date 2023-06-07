755b0b5bba05dcf1a213c369533979caca083a51
Daniel Choi <schoiak@connect.ust.hk>
2023-06-07 16:49:37 +0900

    Refactor: 이름 변경 및 함수 집합화
    파일명 변경, 함수 집합 등등

--- src/main/webapp/resources/js/message/message.js
+++ src/main/webapp/resources/js/message/message.js
@@ -20,8 +20,20 @@ let seeMoreModal;
 let chatModalBackdrop;
 
 
+function initializeSelectors() {
+    documentSelector = $(document);
+    chatBox = $('#chatBox')
+    chatBoxWrap = $("#chatBoxWrap")
+    messageInputBox = $("#messageInputBox");
+    noticeBoxContainer = $('#noticeBoxContainer');
+    sendBtn = $("#sendBtn")
+    seeMoreModal = $("#seeMoreModal")
+    chatModalBackdrop = $("#chatModalBackdrop")
+}
+
 async function connectSocket(nickname_param, channel_param, accountType_param) {
     // Socket 연결
+    initializeSelectors();
 
     nickname = nickname_param;
     channel = channel_param;
