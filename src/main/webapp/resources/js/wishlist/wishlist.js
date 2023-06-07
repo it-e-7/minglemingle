92b8e50fb7f5c100a0bee88e4e899c1f8662c3f5
Sangwon Cho <sangcho617@gmail.com>
2023-06-07 12:04:15 +0900

    Fix: ajax 요청에 닉네임, uuid 추가

--- src/main/webapp/resources/js/wishlist/wishlist.js
+++ src/main/webapp/resources/js/wishlist/wishlist.js
@@ -1,15 +1,34 @@
-$('#share-to-chat').on('click', function () {
-    var myWishlist = JSON.parse(sessionStorage.getItem("myWishlist"))
-    console.log(myWishlist)
-    var itemsList = myWishlist.items
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+}
 
+$('#share-to-chat').on('click', function (e) {
+    e.preventDefault();
+
+    var myWishlist = JSON.parse(sessionStorage.getItem("myWishlist"))
+    var itemsList = myWishlist.items
     var filteredArray = itemsList.filter(function(item) {
         return item !== null && item !== undefined;
     });
+
+    const uuid = generateUUID();
+    var memberNickname = $('#memberNickname').val()
+
     console.log(filteredArray)
+    console.log(uuid)
+    console.log(memberNickname)
+
     $.ajax({
         data: {
-            productCodeList: filteredArray
+            wishlistId: uuid,
+            productCodeList: filteredArray,
+            memberNickname: memberNickname
         },
         url: '/wishlist',
         type: 'post',
